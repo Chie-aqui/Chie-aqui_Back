04bb9c5620ed5051fbe80af7bdef8c27d4c8e407
Mateushqms <mateushqms@gmail.com>
2025-11-20 13:20:29 -0300

    feat: configuracao de roteamento e endpoints da API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,25 +1,28 @@
-# urls.py (do app)
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-app_name = 'empresas'
+app_name = 'api'
 
-# Configuração do ViewSet (CRUD)
 router = DefaultRouter()
-router.register(r'empresas', views.EmpresaViewSet, basename='empresa')
+router.register(r'usuarios', views.UsuarioViewSet, basename='usuario')
+router.register(r'consumidores', views.UsuarioConsumidorViewSet, basename='consumidor')
+router.register(r'empresas', views.UsuarioEmpresaViewSet, basename='empresa')
+router.register(r'administradores', views.AdministradorViewSet, basename='administrador')
+router.register(r'reclamacoes', views.ReclamacaoViewSet, basename='reclamacao')
+router.register(r'respostas_reclamacao', views.RespostaReclamacaoViewSet, basename='resposta_reclamacao')
 
 urlpatterns = [
-    # Cadastro de empresa (endpoint personalizado)
-    path('cadastro/', views.EmpresaCadastroView.as_view(), name='empresa-cadastro'),
-    
-    # Login/Logout (endpoints personalizados)
-    path('login/', views.empresa_login, name='empresa-login'),
-    path('logout/', views.empresa_logout, name='empresa-logout'),
-    
-    # Perfil da empresa (endpoints personalizados)
-    path('perfil/', views.EmpresaPerfilView.as_view(), name='empresa-perfil'),
-    
-    # Inclui as URLs do ViewSet (listar empresas, etc.)
+    path('empresas/cadastro/', views.UsuarioEmpresaCadastroView.as_view(), name='empresa-cadastro'),
+    path('empresas/login/', views.usuario_empresa_login, name='empresa-login'),
+    path('empresas/logout/', views.usuario_empresa_logout, name='empresa-logout'),
+    path('empresas/perfil/', views.UsuarioEmpresaPerfilView.as_view(), name='empresa-perfil'),
+    path('empresas/lista/', views.UsuarioEmpresaListView.as_view(), name='empresa-list'),
+
+    path('consumidores/login/', views.usuario_consumidor_login, name='consumidor-login'),
+    path('consumidores/logout/', views.usuario_consumidor_logout, name='consumidor-logout'),
+    path('consumidores/perfil/', views.UsuarioConsumidorPerfilView.as_view(), name='consumidor-perfil'),
+
+    path('', views.api_root),
     path('', include(router.urls)),
 ]

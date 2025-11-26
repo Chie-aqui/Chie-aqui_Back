1ca664eb8ec3ce4d6452ba1756dbd460d6558dd1
Mateushqms <mateushqms@gmail.com>
2025-11-26 18:35:26 -0300

    ajustes deploy

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -32,16 +32,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',       # loopback
     'localhost',       # localhost
     'chie-aqui-back.onrender.com',  # domínio de produção
-    'chie-aqui-back-1.onrender.com'
     '127.0.0.0'
 ]
 
 # Application definition
-
-SUPERUSER_USERNAME = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-SUPERUSER_EMAIL = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-SUPERUSER_PASSWORD = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'sua_senha_secreta')
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
